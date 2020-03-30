1c953c33208e5089a19b144db529fff72a9ce688
Tese Ogbeifun <tese.og@hotmail.com>
2020-03-30 12:39:03 +0100

    added an empty home php page

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        
+    </body>
+</html><?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
