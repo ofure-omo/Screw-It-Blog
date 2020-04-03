9ae5a4869d205c437f907c2fdc84b6ce015daa37
scfoster91 <scfoster91@gmail.com>
2020-04-03 16:23:53 +0100

    Updated Homepage + added Homepage CSS File

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,22 +1,97 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <title>Screw it | Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <head>
-        <title> Screw it: HOME </title>
-        <link rel="stylesheet" href="styles.css"> 
-    </head>
-    <body>
-        <p>sssjsjsjsjsjsjsjjssjsjs</p>
+    <link rel="stylesheet" href="../css/stylesHome.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<center>
+    
+    <!-- Carousel - Latest Blogs -->
+
+    <div class="container">
+        <h2>Latest Blogs</h2>  
         
-        <?php 
-    include ('Footer.php');
-    ?>
-    </body>
-</html><?php
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            
+        <!-- Indicators -->
+            <!--
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            -->
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        <!-- Wrapper for slides -->
+            <div class="carousel-inner">
 
+                <!--Image 1-->
+                <div class="item active">
+                    <!--Image-->
+                    <img src="https://lovegrowswild.com/wp-content/uploads/2019/05/Simple-DIY-Coffee-Table-18.jpg" alt="Blog text here" style="height:360px;">
+                    <!--Text-->
+                    <div class="absolute-div" id="Favourite1" style="background-color: black;">
+                        <div class="carousel-caption">
+                            <h3 style="background-color: black; opacity: .65">Title Placeholder</h3>
+                            <p style="background-color: white; opacity: .80">Title Placeholder</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Image 2-->
+                <div class="item">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/71V-COkrJcL._SL1500_.jpg" alt="Chicago" style="width:100%;">
+                    <!--Text-->
+                    <div class="absolute-div">
+                        <div class="carousel-caption">
+                            <h3 style="background-color: black; opacity: .65">Title Placeholder</h3>
+                            <p style="color: black; background-color: white; opacity: .80">Blog text<br>etc<br>etc<br>etc<br>etc</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Image 3-->
+                <div class="item">
+                    <img src="https://www.organicauthority.com/.image/t_share/MTU5MzMwNjM3NzM1MDExNTQy/staircase-ideas.jpg" alt="New york" style="width:100%;">
+                    <!--Text-->
+                        <div class="carousel-caption">
+                            <h3 style="background-color: black; opacity: .65">Title Placeholder</h3>
+                            <p style="background-color: white; opacity: .80">Title Placeholder<br>etc<br>etc<br>etc<br>etc</p>
+                        </div>
+                </div>
+            </div>
+
+        <!-- Left and right controls -->
+
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev" style="background-image: none;">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        
+        <a class="right carousel-control" href="#myCarousel" data-slide="next" style="background-image: none;">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
+        </a>
+        
+      </div>
+    </div>
+
+    <br><br>
+    
+ 
+</center>
+
+<?php
+include_once "Footer.php";
+?>
+</body>
+</html>
