de366f9b86793c9cf83cca7b8a763a5685a77fb4
Brannon, Alex <alex.brannon@sky.uk>
2020-04-04 13:35:17 +0100

    Updates to make home work. moved db to sep folder

--- views/pages/home.php
+++ views/pages/home.php
@@ -36,7 +36,7 @@
             <div class="carousel-inner">
 
                 <!--Image 1-->
-                <div class="item active">
+                <div class="carousel-item active">
                     <!--Image-->
                     <img src="https://lovegrowswild.com/wp-content/uploads/2019/05/Simple-DIY-Coffee-Table-18.jpg" alt="Blog text here" style="height:360px;">
                     <!--Text-->
@@ -49,7 +49,7 @@
                 </div>
 
                 <!--Image 2-->
-                <div class="item">
+                <div class="carousel-item">
                     <img src="https://images-na.ssl-images-amazon.com/images/I/71V-COkrJcL._SL1500_.jpg" alt="Chicago" style="width:100%;">
                     <!--Text-->
                     <div class="absolute-div">
@@ -61,7 +61,7 @@
                 </div>
 
                 <!--Image 3-->
-                <div class="item">
+                <div class="carousel-item">
                     <img src="https://www.organicauthority.com/.image/t_share/MTU5MzMwNjM3NzM1MDExNTQy/staircase-ideas.jpg" alt="New york" style="width:100%;">
                     <!--Text-->
                         <div class="carousel-caption">
@@ -73,12 +73,12 @@
 
         <!-- Left and right controls -->
 
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev" style="background-image: none;">
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev" style="background-image: none;">
           <span class="glyphicon glyphicon-chevron-left"></span>
           <span class="sr-only">Previous</span>
         </a>
         
-        <a class="right carousel-control" href="#myCarousel" data-slide="next" style="background-image: none;">
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next" style="background-image: none;">
           <span class="glyphicon glyphicon-chevron-right"></span>
           <span class="sr-only">Next</span>
         </a>
