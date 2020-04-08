e0d2246173b027f6e303ee3470d2f086a5a624a7
LACarlin <linzicarlin@gmail.com>
2020-04-08 11:35:24 +0100

    Commit so I can pull. Still work in progress.

--- models/Login.php
+++ models/Login.php
@@ -12,6 +12,37 @@
  * @author linzicarlin
  */
 class Login {
-    
-    
+
+    public $username;
+    public $password;
+
+    function __construct($username, $password) {
+
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public static function checkPassword() {
+
+        $db = Screw_it::getInstance();
+
+        if (isset($_POST['username']) && $_POST['username'] != "") {
+            $filteredUsername = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+        }
+        if (isset($_POST['password']) && $_POST['password'] != "") {
+            $filteredPassword = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+            //$hashedPassword = password_hash($filteredPassword, PASSWORD_BCRYPT); //creates a password hash 
+        }
+        
+        $username = $filteredUsername;
+        $password = $filteredPassword;
+        
+        $stmt = $db->prepare("SELECT username, password FROM Users WHERE username = :username");
+
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":password", $password);
+
+
+    }
+
 }
