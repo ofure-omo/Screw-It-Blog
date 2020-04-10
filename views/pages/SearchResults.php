09e0f6e9a5bb68d3edc50f1e255b0b133e3aeb3d
Brannon, Alex <alex.brannon@sky.uk>
2020-04-10 15:29:47 +0100

    Updates to header, footer and search

--- views/pages/SearchResults.php
+++ views/pages/SearchResults.php
@@ -1,37 +1,11 @@
 <?php 
 if (count($results) == 1) {
-echo "<span class='results'><br><br><h4>There is " . count($results) . " search result for: " . $_POST['search'] . "</h4></span>";
+echo "<span class='results'><br><br><h4>There is " . count($results) . " search result for: " . $userentry . "</h4></span>";
 } elseif (count($results) > 0) {
-echo "<span class='results'><br><br><h4>There are " . count($results) . " search results for: " . $_POST['search'] . "</h4></span>";
+echo "<span class='results'><br><br><h4>There are " . count($results) . " search results for: " . $userentry . "</h4></span>";
 } else { echo "<center><span class='results'><h4><br>Sorry, there are no results matching your search.<br><h4></span></center>"; }
 ?>
 <br>
-      
-<?php foreach ($results as $blogpost) : ?>
-  
-    <div class="row">
-        <div class="col-sm-6">
-            <div class="card text-center" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image">
-                <div class="card-body">
-                    <h5 class="card-title"><?php echo $blogpost['title'] ?></h5>
-                    <p class="card-text"><?php $blogpostshort = substr($blogpost['body'], 0, 150);
-    echo $blogpostshort . "..." ?></p>  
-                    <a href='?controller=blog&action=read&blog_id=<?php echo $blogpost['blog_id']; ?>' class="btn btn-primary">Read More</a>
-                </div>
-                <div class="card-footer">
-                    <p class="text-muted"><?php
-                        $d = strtotime($blogpost['date_posted']);
-                        echo "Posted on " . date("jS F Y", $d) . "<br>";
-                        ?></p>
-                </div>
-            </div>
-           </div>
-    </div>
-            <br>
-          
-<?php endforeach; ?>
-
 
 
    <?php       
@@ -45,40 +19,37 @@ foreach ($results as $blogpost) : ?>
 </html>
 
  
-    
+  
 
-
-</div>
-
-<div class="card-deck">
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+            
+  <div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-4">
+  <?php foreach ($results as $blogpost) : ?>
+            <div class="card text-center">
+                <img class="card-img-top" src="<?php echo $blogpost['main_image'] ?>" height="100" width="100" >
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $blogpost['title'] ?></h5>
+                    <p class="card-text"><?php $blogpostshort = substr($blogpost['body'], 0, 150);
+    echo $blogpostshort . "..." ?></p>  
+                    <a href='?controller=blog&action=read&blog_id=<?php echo $blogpost['blog_id']; ?>' class="btn btn-primary">Read More</a>
+                </div>
+                <div class="card-footer">
+                    <p class="text-muted"><?php
+                        $d = strtotime($blogpost['date_posted']);
+                        echo "Posted on " . date("jS F Y", $d) . "<br>";
+                        ?></p>
+                </div>
+                
+            </div>
+            <br>
+            <?php endforeach; ?>
+           </div>
+        
     </div>
   </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
+            <br>
+            <br>
+            <br>
+            
+            
--- views/pages/error.php
+++ views/pages/error.php
@@ -1,6 +1,5 @@
                 <section class="main-section">
                         <h5>Oops, something went wrong.</h5>
-                        <img src="./Images/error.jpg" width="500" />
                         
                     <p> Click <a href="?controller=home&action=home">here </a> to return to the home page.</p>
                     </div>
