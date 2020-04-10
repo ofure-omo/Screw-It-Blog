5907df653ed913eef679ca926e19ab2078b0fc58
AmritaKB <bains.amrita.k@gmail.com>
2020-04-10 18:56:24 +0100

    removal of prepopulate view with existing details

--- views/pages/userUpdate.php
+++ views/pages/userUpdate.php
@@ -1,10 +1,4 @@
-<?php
-/****PRE POPULATE EXISTING USER DETAILS
-$stmt = $pdo->prepare('SELECT * FROM Users WHERE user_ID = ?');
-    $stmt->execute([$_GET['user_ID']]);
-    
-    $contact = $stmt->fetch(PDO::FETCH_ASSOC);****/
-?>
+
 
 <!DOCTYPE html>
 
