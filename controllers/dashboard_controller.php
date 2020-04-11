5fed345b449adaf4c2cad102f8c9b4d8c7142309
AmritaKB <bains.amrita.k@gmail.com>
2020-04-11 21:49:49 +0100

    no message

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -8,7 +8,31 @@ class dashboardcontroller {
     //insert IF statement to direct to either member dashboard or blogger dashboard
      
     public function mem_details (){
-        if(!isset($_SESSION['user_id'])) {
+        if(!isset($_SESSION['user_id'])) {   
+      
+            $details = dashboard::getDetails(($_SESSION['user_id']));
+            require_once('views/pages/mem_dash.php');
+            echo $_SESSION["user_type"];
+
+    } else { return call('pages', 'error');
+        
+    }
+}
+
+    public function fav_blog (){
+        if(!isset($_SESSION['user_id'])) {   
+      
+            $details = dashboard::getDetails(($_SESSION['user_id']));
+            require_once('views/pages/mem_dash.php');
+            echo $_SESSION["user_type"];
+
+    } else { return call('pages', 'error');
+        
+    }
+}
+
+    public function comments (){
+        if(!isset($_SESSION['user_id'])) {   
       
             $details = dashboard::getDetails(($_SESSION['user_id']));
             require_once('views/pages/mem_dash.php');
