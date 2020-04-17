7f52c295b7c45130f8d30b761a38b6c5676ff9b6
AmritaKB <bains.amrita.k@gmail.com>
2020-04-17 07:55:42 +0100

    no message

--- Tese/new files/models/dashboard.php
+++ Tese/new files/models/dashboard.php
@@ -4,21 +4,20 @@ class dashboard {
 
     public function getDetails($user_id) {
         $db = Screw_it::getInstance();
-        
+
         $user_id = intval($user_id);
-        
+
         $req = $db->prepare('SELECT * FROM users WHERE user_id = :user_id;');
-        
-                if (!$req) {
-                    echo "error, pls handle";
-                }
+
+        if (!$req) {
+            echo "error, pls handle";
+        }
 
         $req->execute(array('user_id' => $user_id));
         $mem_details = $req->fetch();
         $details = $mem_details;
 
         return $details;
-  
     }
 
     public function getFavourites($user_id) {
@@ -34,9 +33,8 @@ class dashboard {
         $stmt->execute(array('user_id' => $user_id));
         $favourites = $stmt->fetchAll();
 
-        return $favourites; 
-        
-}
+        return $favourites;
+    }
 
     public function getComments() {
         $db = Screw_it::getInstance();
@@ -56,39 +54,43 @@ class dashboard {
         return $comms;
     }
 
-    
-   public function deleteUser(){
-       if (isset($_GET['user_ID'])) {
-	$id = $_GET['user_ID'];
-	$pdo->query("DELETE FROM Users WHERE user_ID= " . $id . "; ");
-	echo "<div class='msg'>
+    public function deleteUser($user_id) {
+        $db = Screw_it::getInstance();
+        $user = intval($user_id);
+        if (isset($_GET['user_ID'])) {
+            $id = $_GET['user_ID'];
+            $pdo->query('delete FROM Users WHERE user_id = :user_id');
+            $req->execute(array('user_id'=>$user));
+            echo "<div class='msg'>
         <h2 class='del'>User has been deleted</h2>
         </div>";
-
-}
+        }
         $deleteUser = $req->fetch();
         return $deleteUser;
-}
-   public function unfavourite(){
-       if (isset($_GET['user_ID'])) {
-	$id = $_GET['user_ID'];
-	$pdo->query("DELETE FROM favourites WHERE (user_ID= " . $id . "& blog_ID= " . $blog_id ."; ");
-	echo "<div class='msg'>
+    }
+
+    public function unfavourite() {
+        if (isset($_GET['user_ID'])) {
+            $id = $_GET['user_ID'];
+            $pdo->query("DELETE FROM favourites WHERE (user_ID= " . $id . "& blog_ID= " . $blog_id . "; ");
+            echo "<div class='msg'>
         <h2 class='del'>Favourite has been deleted</h2>
         </div>";
-       }
-       $unfavourite = $req->fetch();
-       return $unfavourite;
-}
-  public function deleteComment(){
-       if (isset($_GET['user_ID'])) {
-	$id = $_GET['user_ID'];
-	$pdo->query("DELETE FROM comments WHERE (user_ID= " . $id . "& comment_ID= " . $comment_id ."; ");
-	echo "<div class='msg'>
+        }
+        $unfavourite = $req->fetch();
+        return $unfavourite;
+    }
+
+    public function deleteComment() {
+        if (isset($_GET['user_ID'])) {
+            $id = $_GET['user_ID'];
+            $pdo->query("DELETE FROM comments WHERE (user_ID= " . $id . "& comment_ID= " . $comment_id . "; ");
+            echo "<div class='msg'>
         <h2 class='del'>Comment has been deleted</h2>
         </div>";
-       }
-       $deleteComment=$req->fetch();
-       return $deleteComment;
-  }
+        }
+        $deleteComment = $req->fetch();
+        return $deleteComment;
+    }
+
 }
--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -1,69 +1,84 @@
 <?php
 
+include 'Users.php';
 
 class dashboardcontroller {
-  
-     
-    public function mem_details (){
 
-        if(isset($_SESSION['user_id'])) {   
-      
+    public function mem_details() {
+
+        if (isset($_SESSION['user_id'])) {
+            $details = dashboard::getDetails(($_SESSION['user_id']));
+            $comms = dashboard::getCountComments(($_SESSION['user_id']));
+            $commenttext = dashboard::getComments(($_SESSION['user_id']));
+            require_once('views/pages/mem_dash.php');
+            echo $_SESSION["user_type"];
+            
+        } if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $id = $details[0];
+            Dashboardcontroller::updateProfile($id);
+            require_once('views/pages/mem_dash.php');
+        } else {
+            return call('pages', 'error');
+        }
+    }
+        public function delete() {
+            Blogger::deleteAccount($_GET['user_id']);
+          //  require_once('Views/pages/home.php');
+
+      }
+    /*
+    public function fav_blog() {
+        if (isset($_SESSION['user_id'])) {
+
             $details = dashboard::getDetails(($_SESSION['user_id']));
             require_once('views/pages/mem_dash.php');
             echo $_SESSION["user_type"];
-
-    } else { return call('pages', 'error');
-        
+        } else {
+            return call('pages', 'error');
+        }
     }
-}
 
-    public function fav_blog (){
-        if(isset($_SESSION['user_id'])) {   
-      
+    public function comments() {
+        if (isset($_SESSION['user_id'])) {
             $details = dashboard::getDetails(($_SESSION['user_id']));
             require_once('views/pages/mem_dash.php');
             echo $_SESSION["user_type"];
+        } else {
+            return call('pages', 'error');
+        }
+    }
 
-    } else { return call('pages', 'error');
-        
+    public function deleteUser() {
+        if (isset($_SESSION['user_id'])) {
+            $details = dashboard::getDetails(($_SESSION['user_id']));
+            require_once('views/pages/mem_dash.php');
+            echo $_SESSION["user_type"];
+        } else {
+            return call('pages', 'error');
+        }
     }
-}
 
-    public function comments (){
-        if(isset($_SESSION['user_id'])) {
+    public function unfavourite() {
+        if (isset($_SESSION['user_id'])) {
             $details = dashboard::getDetails(($_SESSION['user_id']));
             require_once('views/pages/mem_dash.php');
-         echo $_SESSION["user_type"];
-    } else { return call('pages', 'error');
-     
+            echo $_SESSION["user_type"];
+        } else {
+            return call('pages', 'error');
+        }
     }
-}
 
-   public function deleteUser(){
-        if(isset($_SESSION['user_id'])) {
+    public function deleteComment() {
+        if (isset($_SESSION['user_id'])) {
             $details = dashboard::getDetails(($_SESSION['user_id']));
             require_once('views/pages/mem_dash.php');
-         echo $_SESSION["user_type"];
-    } else { return call('pages', 'error');
+            echo $_SESSION["user_type"];
+        } else {
+            return call('pages', 'error');
+        }
     }
-    }
-    
-   public function unfavourite(){
-        if(isset($_SESSION['user_id'])) {
-            $details = dashboard::getDetails(($_SESSION['user_id']));
-            require_once('views/pages/mem_dash.php');
-         echo $_SESSION["user_type"];
-    } else { return call('pages', 'error');
-    }
-    }
-    
-  public function deleteComment(){
-        if(isset($_SESSION['user_id'])) {
-            $details = dashboard::getDetails(($_SESSION['user_id']));
-            require_once('views/pages/mem_dash.php');
-         echo $_SESSION["user_type"];
-    } else { return call('pages', 'error');
-     
-    }
+
 }
-}
+     * /*
+     * 
+     */
